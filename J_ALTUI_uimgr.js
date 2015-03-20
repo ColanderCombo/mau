8258143b0b911700080eb83235b749f529faa496
amg0 <amg0@450a5c96-c833-4324-acf6-e5f578b7b714>
2015-03-20 18:27:04 +0000

    new code repository

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -1,4 +1,4 @@
-//# sourceURL=J_ALTUI_uimgr.js
+//#  sourceURL=J_ALTUI_uimgr.js
 // http://192.168.1.16:3480/data_request?id=lr_ALTUI_Handler&command=home
 // This program is free software: you can redistribute it and/or modify
 // it under the condition that it is for private or home useage and 
