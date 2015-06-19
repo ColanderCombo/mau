5d542a33207d18d1985e3b0b6a4482f7c0124eb6
amg0 <amg0@450a5c96-c833-4324-acf6-e5f578b7b714>
2015-06-19 08:08:05 +0000

    Prep work for saving columns preferences

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -1,6 +1,6 @@
 //#  sourceURL=J_ALTUI_uimgr.js
 // http://192.168.1.16:3480/data_request?id=lr_ALTUI_Handler&command=home
-// This program is free software: you can redistribute it and/or modify
+// ALTUI: This program is free software: you can redistribute it and/or modify
 // it under the condition that it is for private or home useage and 
 // this whole comment is reproduced in the source code file
 // Commercial utilisation is not authorized without the appropriate
@@ -9,6 +9,27 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 
+/*The MIT License (MIT)
+BOOTGRID: Copyright (c) 2014-2015 Rafael J. Staib
+
+Permission is hereby granted, free of charge, to any person obtaining a copy 
+of this software and associated documentation files (the "Software"), to deal 
+in the Software without restriction, including without limitation the rights 
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in 
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+THE SOFTWARE.
+*/
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
 var _HouseModes = [];
 var deviceModalTemplate = "";
@@ -6555,7 +6576,11 @@ var UIManager  = ( function( window, undefined ) {
 				$(".altui-mainpanel").append( html );
 				$("#altui-grid").bootgrid({
 					caseSensitive: false
-				});
+				}).on("loaded.rs.jquery.bootgrid", function (e)
+				{
+					var settings = $("#altui-grid").bootgrid("getColumnSettings");
+					/* your code goes here */
+				});	
 			}
 		);
 	},
