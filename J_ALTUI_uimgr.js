586f27cea15a24ad131518feea83a53a969b8ce6
amg0 <amg0@450a5c96-c833-4324-acf6-e5f578b7b714>
2015-07-09 12:20:16 +0000

    v341

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -8,7 +8,7 @@
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-
+ 
 /*The MIT License (MIT)
 BOOTGRID: Copyright (c) 2014-2015 Rafael J. Staib
 
